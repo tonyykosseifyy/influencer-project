9fcccdc26cf374f4f83ede4e5af46f1e45fef3d9
Tony Kosseify <tonykosseify123@gmail.com>
2023-01-23 01:44:28 +0200

    add: Website logo to Navbar

--- client/components/elements/navbar.js
+++ client/components/elements/navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from "./navbar.module.css";
+import Logo from "../reusables/Logo";
 
 
 const navbar = () => {
   return (
-    <nav className={styles.navbar}>navbar</nav>
-  )
-}
+    <nav className={styles.navbar}>
+      <Logo />
+    </nav>
+  );
+};
 
 export default navbar
--- client/components/reusables/Logo.js
+++ client/components/reusables/Logo.js
@@ -2,6 +2,7 @@ import styles from "./logo.module.css";
 
 
 const Logo = ({ propsStyles }) => {
+  console.log(styles);
   return (
       <h1 className={styles.logo} style={propsStyles}>Styrate</h1>
   )
